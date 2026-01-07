db72c9f8a9468e969224e745115e76e934144402
jagrati7305 <jagrati07.work@gmail.com>
2026-01-07 19:41:31 +0530

    AppColors Added

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static final Color placeholderColorDark = const Color(0xFF6B69D7);
+  static final Color placeholderColorLight = const Color(0xFFB0A0F5);
+  static final Color darkPurple = const Color(0xFF4632B5);
+  static final Color lightPurple = const Color(0xFF977FFF);
+  static final Color white = const Color(0xFFFFFFFF);
+}
