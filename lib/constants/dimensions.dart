4eb7982811200fa596766c4e2a1cc39bcbeb6ae1
jagrati7305 <jagrati07.work@gmail.com>
2026-01-07 19:45:19 +0530

    Dimensions Added for Responsive Designing

---
+++ lib/constants/dimensions.dart
@@ -0,0 +1,8 @@
+class Dimensions {
+  static late double screenWidth;
+  static late double screenHeight;
+
+static double heightFactor = screenHeight/874;
+static double widthFactor = screenWidth/402;
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bin2dec/constants/dimensions.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Dimensions.screenWidth = MediaQuery.of(context).size.width;
+    Dimensions.screenHeight = MediaQuery.of(context).size.height;
+
     return MaterialApp(
       title: 'Bin2Dec',
+      home: Scaffold(),
     );
   }
 }
