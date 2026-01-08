15c35264d91007587ac2cba5b9a25172f33bbc65
jagrati7305 <jagrati07.work@gmail.com>
2026-01-08 18:23:57 +0530

    Binary to Decimal Conversion Custom Numpad

---
+++ lib/widgets/binary_to_decimal_numpad.dart
@@ -0,0 +1,35 @@
+import 'package:bin2dec/widgets/button.dart';
+import 'package:flutter/material.dart';
+
+class BinaryToDecimalNumpad extends StatefulWidget {
+  const BinaryToDecimalNumpad({super.key});
+
+  @override
+  State<BinaryToDecimalNumpad> createState() => _BinaryToDecimalNumpadState();
+}
+
+class _BinaryToDecimalNumpadState extends State<BinaryToDecimalNumpad> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Button(num: '0'),
+            Button(num: '1')
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Button(num: 'Switch',icon: Icons.swap_vert_rounded,iconsize: 36,),
+            Button(num: 'Delete',icon: Icons.backspace,),
+            
+          ],
+        )
+      ],
+    );
+  }
+}
