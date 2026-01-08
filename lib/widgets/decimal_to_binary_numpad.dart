0c1d2b460a8acc0313880ffe079361f2f38399a9
jagrati7305 <jagrati07.work@gmail.com>
2026-01-08 18:53:52 +0530

    Decimal to Binary Numpad

---
+++ lib/widgets/decimal_to_binary_numpad.dart
@@ -0,0 +1,53 @@
+import 'package:bin2dec/constants/app_color.dart';
+import 'package:bin2dec/widgets/button.dart';
+import 'package:flutter/material.dart';
+
+class DecimalToBinaryNumpad extends StatefulWidget {
+  const DecimalToBinaryNumpad({super.key});
+
+  @override
+  State<DecimalToBinaryNumpad> createState() => _DecimalToBinaryNumpadState();
+}
+
+class _DecimalToBinaryNumpadState extends State<DecimalToBinaryNumpad> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Button(num: '1',numcolor: AppColor.darkPurple,numsize: 28,),
+            Button(num: '2',numcolor: AppColor.darkPurple,numsize: 28,),
+            Button(num: '3',numcolor: AppColor.darkPurple,numsize: 28,),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Button(num: '4',numcolor: AppColor.darkPurple,numsize: 28,),
+            Button(num: '5',numcolor: AppColor.darkPurple,numsize: 28,),
+            Button(num: '6',numcolor: AppColor.darkPurple,numsize: 28,),
+          ],
+        ),  
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Button(num: '7',numcolor: AppColor.darkPurple,numsize: 28,),
+            Button(num: '8',numcolor: AppColor.darkPurple,numsize: 28,),
+            Button(num: '9',numcolor: AppColor.darkPurple,numsize: 28,),
+          ],
+        ), 
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Button(num: 'Switch',icon: Icons.swap_vert_rounded,iconsize: 36,numcolor: AppColor.darkPurple,numsize: 28,),
+            Button(num: '0',numcolor: AppColor.darkPurple,numsize: 28,),
+            Button(num: 'Delete',icon: Icons.backspace,numcolor: AppColor.darkPurple,numsize: 28,),
+          ],
+        ),       
+      ],
+    );
+  }
+}
